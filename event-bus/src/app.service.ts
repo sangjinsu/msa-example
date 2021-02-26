16c3ab924a551e990cd2deb2ccf9500487ec64b9
jinsuSang <twtz0309@naver.com>
2021-02-26 15:30:19 +0900

    feat: update localhost to k8s cluster ip

--- event-bus/src/app.service.ts
+++ event-bus/src/app.service.ts
@@ -2,26 +2,24 @@ import { HttpService, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  private events = [];
+  private static events = [];
   constructor(private httpService: HttpService) {}
   fetchEvent = (event: { type: string; data: any }) => {
-    // await this.httpService
-    //   .post('http://localhost:4000/events', event)
-    //   .toPromise();
-    // await this.httpService
-    //   .post('http://localhost:4001/events', event)
-    //   .toPromise();
-    // await this.httpService
-    //   .post('http://localhost:4002/events', event)
-    //   .toPromise();
-    // await this.httpService
-    //   .post('http://localhost:4003/events', event)
-    //   .toPromise();
-    this.events.push(event);
+    AppService.events.push(event);
+    this.sendEvents(event);
+  };
+
+  sendEvent = () => {
+    return AppService.events;
+  };
+
+  private sendEvents(event: { type: string; data: any }) {
     this.httpService
-      .post('http://localhost:4000/events', event)
+      .post('http://posts-clusterip-srv:4000/events', event)
       .toPromise()
       .catch((err) => console.log(err.message));
+
+    /**
     this.httpService
       .post('http://localhost:4001/events', event)
       .toPromise()
@@ -34,9 +32,6 @@ export class AppService {
       .post('http://localhost:4003/events', event)
       .toPromise()
       .catch((err) => console.log(err.message));
-  };
-
-  sendEvent = () => {
-    return this.events;
-  };
+     */
+  }
 }
